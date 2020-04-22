d2e0ebd83712893f89cd72ac393a950e20472c6a
Ovy95 <jackoverton21@hotmail.com>
2020-04-22 16:42:15 +0100

    contains other names in the array

--- RemoveNames.test.js
+++ RemoveNames.test.js
@@ -2,8 +2,17 @@ const removeJNames = require('./RemoveNames');
 
 describe('removeJNames', function () {
   it('Should remove names that start with J', () => {
-    const names = ['ames', 'Carol','Pete','Jim'];
+    const names = ['James', 'Carol','Pete','Jim'];
     expect(removeJNames(names)).not.toContain('James')
+    expect(removeJNames(names)).not.toContain('Jim')
   });
+
+  it('Should contain the other names in the array', () => {
+    const names = ['James', 'Carol','Pete','Jim'];
+    expect(removeJNames(names)).toContain('Pete')
+    expect(removeJNames(names)).toContain('Carol')
+  });
+
+
 });
 
